9013bff770ef5a94a401fd2d3d45f000301ef5bf
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2025-01-13 13:19:08 +0530

    Update Operators.R

--- Operators.R
+++ Operators.R
@@ -8,10 +8,10 @@
 a<-7.5
 b<-2
 
-print(a+b)
-print(a-b)
-print(a*b)
-print(a/b)
-print(a%%b)
-print(a%/%b)
-print(a^b)
+print(a+b) # Additiom
+print(a-b) # substract
+print(a*b) # Multiplicatin
+print(a/b) #Division
+print(a%%b) #Remainder
+print(a%/%b) # quotient
+print(a^b) # Power
