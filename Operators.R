fc4970ad71cdb34f01305b3ec8f21ca8991edcbf
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2025-01-13 13:17:54 +0530

    Update Operators.R

---
+++ Operators.R
@@ -0,0 +1,17 @@
+## Operators in R
+
+## Arithmetic ops = + - * / %%(remainder) %/%(quotient) ^
+## Relational ops
+## Logical ops
+## Assignment ops
+
+a<-7.5
+b<-2
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%%b)
+print(a%/%b)
+print(a^b)
