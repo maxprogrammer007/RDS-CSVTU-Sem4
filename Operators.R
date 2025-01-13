258bdc06d4975061b10771efc7e4c35ad9c97937
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2025-01-13 13:31:10 +0530

    Update Operators.R

--- Operators.R
+++ Operators.R
@@ -15,3 +15,28 @@ print(a/b) #Division
 print(a%%b) #Remainder
 print(a%/%b) # quotient
 print(a^b) # Power
+
+# Vector is collection of similar data types 
+
+a<-c(8,9,10)
+b<-c(2,3,4)
+
+
+print(a+b) # Additiom
+print(a-b) # substract
+print(a*b) # Multiplicatin
+print(a/b) #Division
+print(a%%b) #Remainder
+print(a%/%b) # quotient
+print(a^b) # Power
+
+
+## Relational Ops -  < >  == <= >= !=  output - TRUE / FALSE
+
+print(a>b) # 
+print(a<b) # 
+print(a==b) # 
+print(a<=b) #
+print(a>=b) 
+print(a!=b) 
+ 
