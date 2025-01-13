60bcf94f1b11038a181e8bb8abbdf80ae0261943
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2025-01-13 13:53:37 +0530

    Update conditionals.R

--- conditionals.R
+++ conditionals.R
@@ -8,3 +8,20 @@ if(is.integer(x))
 {
   print(" X is not an integer number")
 }
+
+y<-c("Hardwork","is","the","Key","to","success")
+y
+if("Key"%in%y)
+{
+  print("yes its present in the vector")
+
+}else{
+  print("not found")
+}
+if("Ram"%in%y)
+{
+  print("yes its present in the vector")
+  
+}else{
+  print("not found")
+}
