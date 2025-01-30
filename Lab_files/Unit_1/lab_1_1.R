c1b933e40ff249f7ce9cb5985bd982bc95fecbda
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2025-01-30 11:48:43 +0530

    Create lab_1_1.R

---
+++ Lab_files/Unit_1/lab_1_1.R
@@ -0,0 +1,13 @@
+## Basic Mathematical Computations 
+
+a <- 10
+aa <-sqrt(a) # square root
+print(aa)
+
+b <- 15 
+square <- b * b # square 
+print(square)
+
+c <- 20 
+exponent <- exp(c) # exponent
+print(exponent)
