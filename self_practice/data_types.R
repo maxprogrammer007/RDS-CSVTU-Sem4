30799ae612b6f048ecde0d3b6178e785903865c7
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2025-01-30 11:47:22 +0530

    Create data_types.R

---
+++ self_practice/data_types.R
@@ -0,0 +1,16 @@
+## data types in R
+
+## logical , numeric , integer , complex , character , raw
+
+num <- 10 ## default data type will be numeric and numeric includes like 12 , 1234 , -24 , 12.356 
+
+integer <- 10L ## need to provide L in suffix 
+
+complex = 5 + 2i 
+
+## logical = TRUE , FALSE
+
+character = 'a'
+character_2 = " hello "
+# Both  single and double quotes are available to use 
+
