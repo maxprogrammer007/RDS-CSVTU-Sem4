15bcc7b040bae2dfb88318f1e5ff5aa7a38f2c4a
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2025-01-30 11:48:56 +0530

    Create lab_1_5.R

---
+++ Lab_files/Unit_1/lab_1_5.R
@@ -0,0 +1,10 @@
+## Confirm the length of the vector created is 20 
+
+# creating the vector of length 20 
+
+vect<- seq(from= 1, to = 20 ,by=1)
+print(vect)
+
+# finding the length of the vector 
+
+print(length(vect))
