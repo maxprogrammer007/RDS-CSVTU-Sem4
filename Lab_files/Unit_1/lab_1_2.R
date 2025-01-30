c89210bc70ed56e121fe35a969baafbca3f07069
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2025-01-30 11:48:48 +0530

    Create lab_1_2.R

---
+++ Lab_files/Unit_1/lab_1_2.R
@@ -0,0 +1,17 @@
+## Create an object C that stores the value then overwrite the object in by itself divided by Y. Print the result to the console
+
+## Creating an object 
+
+X <- 45
+print(X)
+
+# defining the Y variable 
+
+Y <- 10 
+print(Y)
+
+
+# Overwriting the X 
+
+X <- X / Y 
+print(X)
