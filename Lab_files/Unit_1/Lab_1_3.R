b7c85c72bdd24ed19a627dba52183f60551d73b7
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2025-01-30 11:48:51 +0530

    Create Lab_1_3.R

---
+++ Lab_files/Unit_1/Lab_1_3.R
@@ -0,0 +1,12 @@
+## Create and store a sequence of value from x to y that progresses in steps of 0.3 
+
+#  defining the variables X and Y
+
+x <- 1
+
+y <- 20 
+
+# creating a new variable which will perform the given task 
+
+f <- seq(from = x , to = y , by = 0.3)
+print(f)
