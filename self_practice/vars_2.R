2c70edbc71e280ffe9edea0ab8c8251b5c9c523a
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2025-01-30 11:47:32 +0530

    Create vars_2.R

---
+++ self_practice/vars_2.R
@@ -0,0 +1,16 @@
+## variables in R programming language 
+
+var_name <- 10
+var.name <- 10
+
+print(var.name+var_name)
+
+## Assignment of variables
+
+var1 <- 10
+var2 = 20
+10 -> var3
+
+print(var1 + var2+var3)
+
+cat(var1," ",var2,"\n", var3)
