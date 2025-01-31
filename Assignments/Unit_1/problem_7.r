42c961b829c4d7831dceb07326e33bf810d7d540
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2025-01-31 07:06:43 +0530

    Create problem_7.r

---
+++ Assignments/Unit_1/problem_7.r
@@ -0,0 +1,8 @@
+#Q7. Extract the first and last elements of your vector from (Q6.),
+#      storing them as a new object.
+
+
+X4 <- Sol6[1]
+X5 <- Sol6[length(Sol6)]
+Sol7 <- c(X4 , X5)
+print(Sol7)
